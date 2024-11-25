9de114742bf2cf38198fdcfad404d50d87e17f57
Yaroslav <yariksimakov@mail.ru>
2024-11-25 12:02:05 +0300

    Work by excel

---
+++ settings/settings.py
@@ -0,0 +1,6 @@
+
+
+SHEET_NAME: str = 'Сопроводительный лист'
+
+DIRECTION_BY_SAVE_DATA_PATH = ''
+LINE_EDIT = 'linEdit_excel_file.pkl'
--- working_with_excel_file.py
+++ working_with_excel_file.py
@@ -1,15 +1,29 @@
-from pprint import pprint
-import pandas as pd
+import os
+
 import openpyxl
 
-excel_file_old: str = 'Krasnopol'
-expanded: str = '.xlsm'
-sheet_name = 'Сопроводительный лист'
+
+test_direction: str = r'test direction\\'
+file_name = 'Krasnopol'
+expanded: str = '.xlsx'
 
 
 # data = pd.read_excel(excel_file_old + expanded)
 # data.to_excel(excel_file_old + '.2' + expanded)
 
+class GetExpandedFile:
+	def get_index_for_last_file(self, direction:str) -> int:
+		"""
+		:return: -1 if index is Not else integer index
+		"""
+		set_files = set(os.listdir(direction))
+		for index in range(101):
+			if index != 0:
+				file = file_name + f'.{index}' + expanded
+			else:
+				file = file_name + expanded
+			if not file in set_files:
+				return index - 1
 
 class ModifyExistingExcelFile:
 	def __init__(self, name_excel_file, name_sheet='Сопроводительный лист'):
@@ -24,13 +38,18 @@ class ModifyExistingExcelFile:
 
 
 def test_start(object_class):
-	change_excel_file = object_class(excel_file_old + expanded)
-	name_cells = (f'B{el}' for el in range(10, 18))
-	value_cells = ('05.015.09', '-B-C', 13560, '6R777', 660, '4M', 780, 777)
-	for index, name_cell in enumerate(name_cells):
-		change_excel_file.mode_excel_file(name_cell, value_cells[index])
-	change_excel_file.save_changed_excel_file(excel_file_old + '.2' + '.xlsx')
+	excel_file = object_class(test_direction + file_name + expanded)
+	# name_cells = (f'B{el}' for el in range(10, 18))
+	# value_cells = ('05.015.09', '-B-C', 13560, '6R777', 660, '4M', 780, 777)
+	# for index, name_cell in enumerate(name_cells):
+	# 	change_excel_file.mode_excel_file(name_cell, value_cells[index])
+	for index in range(1, 5):
+		excel_file.save_changed_excel_file(test_direction + file_name + f'.{index}' + '.xlsx')
 
 
 if __name__ == '__main__':
-	pass
+	# test_start(ModifyExistingExcelFile)
+	test = GetExpandedFile().get_index_for_last_file(
+		test_direction)
+	print(test)
+	# pass
